8e65781ad87e757753837dfdd60a5ce0c0a31aa2
Pablo Rios <pablorios1603@gmail.com>
2020-05-20 16:59:49 -0500

    Se agrego la pantalla de inicio (aun no acabada)

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,20 +1,26 @@
-
-const routes = [
-  {
+const routes = [{
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Index.vue') }
-    ]
-  }
-]
+    component: () =>
+        import ('layouts/MainLayout.vue'),
+    children: [{
+        path: '',
+        component: () =>
+            import ('pages/Index.vue')
+    }]
+}, {
+    path: '/inicio',
+    component: () =>
+        import ('pages/inicio.vue'),
+}]
 
 // Always leave this as last one
 if (process.env.MODE !== 'ssr') {
-  routes.push({
-    path: '*',
-    component: () => import('pages/Error404.vue')
-  })
+    routes.push({
+        path: '*',
+        component: () =>
+            import ('pages/Error404.vue')
+    })
 }
+
 
 export default routes
