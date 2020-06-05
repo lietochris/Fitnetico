a403407843d2cd5119226e614ad821cd1808bb8b
AABirthday <61792725+AABirthday@users.noreply.github.com>
2020-06-04 21:06:43 -0500

    Se agrego Bienvenida

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,21 @@
 const routes = [
   {
     path: '/',
-    component: () => import('pages/Index.vue'),
+    component: () => 
+    import('pages/Index.vue'),
+    children: [{
+      path: '',
+      component: () => 
+      import('pages/Index.vue'),
+    }]
+
+  },
+  {
+    path: '/Bienvenida',
+    component: () => 
+      import ('pages/Bienvenida.vue'),
   }
-];
+]
 
 // Always leave this as last one
 if (process.env.MODE !== 'ssr') {
