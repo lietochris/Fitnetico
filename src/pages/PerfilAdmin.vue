707571ef06e3b2435642c7723763e1c203e2f20d
Uriel Jimenez Zempoalteca <uaemexzempo@gmail.com>
2020-06-15 23:22:43 -0500

    Form
    Se insertaron 'form' a las paginas  de crear ejercicio y perfil admin

--- src/pages/PerfilAdmin.vue
+++ src/pages/PerfilAdmin.vue
@@ -19,12 +19,39 @@
         <div class="tw-flex tw-justify-center recentA">
           <div class="tw-w-1/4"></div>
           <div class="tw-w-2/4">
-                <q-input color="cyan-5" input-class="text-white tw-justify-center" label-color="white" class="text-white tw-pt-8" v-model="nombre" label="Nombre:" style="max-width: 800px" />
-                <q-input color="cyan-5" input-class="text-white tw-justify-center" label-color="white" class="text-white tw-pt-8" v-model="correo" label="Correo Electronico:" style="max-width: 800px" />
-                <q-input color="cyan-5" input-class="text-white tw-justify-center" label-color="white" class="text-white tw-pt-8" v-model="telefono" label="Telefono:" style="max-width: 800px" />
-                <q-input color="cyan-5" input-class="text-white tw-justify-center" label-color="white" class="text-white tw-pt-8" v-model="direccion" label="Dirección:" style="max-width: 800px" />
-                <q-input color="cyan-5" input-class="text-white tw-justify-center" label-color="white" class="text-white tw-pt-8" v-model="descripcion" label="Descripción:" style="max-width: 800px " filled type="textarea"/>
+            <q-form @submit="onSubmit2" @reset="onReset2">
+                <q-input lazy-rules
+                :rules="[ val => val && val.length > 0 || 'Porfavor llene el campo']"
+                color="cyan-5" input-class="text-white tw-justify-center" label-color="white"
+                class="text-white tw-pt-8" v-model="nombre" label="Nombre:" style="max-width: 800px" />
+                
+                <q-input lazy-rules
+                :rules="[ val => val && val.length > 0 || 'Porfavor llene el campo']"
+                color="cyan-5" input-class="text-white tw-justify-center" label-color="white"
+                class="text-white tw-pt-8" v-model="correo" label="Correo Electronico:" style="max-width: 800px" />
+                
+                <q-input lazy-rules
+                :rules="[ val => val && val.length > 0 || 'Porfavor llene el campo']"
+                color="cyan-5" input-class="text-white tw-justify-center" label-color="white" 
+                class="text-white tw-pt-8" v-model="telefono" label="Telefono:" style="max-width: 800px" />
+                
+                <q-input lazy-rules
+                :rules="[ val => val && val.length > 0 || 'Porfavor llene el campo']"
+                color="cyan-5" input-class="text-white tw-justify-center" label-color="white" 
+                class="text-white tw-pt-8" v-model="direccion" label="Dirección:" style="max-width: 800px" />
+                
+                <q-input lazy-rules
+                :rules="[ val => val && val.length > 0 || 'Porfavor llene el campo']"
+                color="cyan-5" input-class="text-white tw-justify-center" label-color="white" 
+                class="text-white tw-pt-8" v-model="descripcion" label="Descripción:" style="max-width: 800px " filled type="textarea"/>
+                
                 <p></p>
+                
+                <div class="tw-my-8">
+                  <q-btn label="Guardar" type="submit" color="secondary" rounded/>
+                  <q-btn label="Reset" type="reset" color="white" flat class="q-ml-sm" />
+                </div>
+            </q-form>
           </div>
           <div class="tw-w-1/4"></div>
         </div>
@@ -47,6 +74,35 @@
       telefono: '5555555555',
       direccion: 'Avenida siempre viva',
       descripcion: 'Mamadisima',
+    }
+  },
+
+  methods: {
+    onSubmit2 () {
+      if (this.nombre == null || this.correo == null || this.telefono == null || this.direccion == null || this.descripcion == null) {
+        this.$q.notify({
+          color: 'red-5',
+          textColor: 'white',
+          icon: 'warning',
+          message: 'Termina de llenar los campos'
+        })
+      }
+      else {
+        this.$q.notify({
+          color: 'green-4',
+          textColor: 'white',
+          icon: 'cloud_done',
+          message: 'Guardado'
+        })
+      }
+    },
+
+    onReset2 () {
+      this.nombre = null
+      this.correo = null
+      this.telefono = null
+      this.direccion = null
+      this.descripcion = null
     }
   }
 }
