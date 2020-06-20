c73e3ae82b4f0ef0a0b24a985ec85c61b6c416e2
Pablo Rios <pablorios1603@gmail.com>
2020-06-19 19:12:53 -0500

    Se arreglo el bug que no permitia ver las paginas

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,87 +1,107 @@
-const routes = [
-  {
-    component: () => import('pages/Index.vue')
-  },
-    
-      {
-    path: '/Bienvenida',
-    component: () => 
-      import ('pages/Bienvenida.vue'),
-  },
+const routes = [{
+        path: '/',
+        component: () =>
+            import ('pages/Index.vue')
+    },
 
-  {
-    path: '/Expertos',
-    component: () => 
-      import ('pages/Expertos.vue'),
-  },
+    {
+        path: '/bienvenida',
+        component: () =>
+            import ('pages/Bienvenida.vue'),
+    },
 
-  {
-    path: '/Registro',
-    component: () => 
-      import ('pages/Registro.vue'),
-  },
+    {
+        path: '/expertos',
+        component: () =>
+            import ('pages/Expertos.vue'),
+    },
 
-  {
-    path: '/Login',
-    component: () => 
-      import ('pages/Login.vue'),
-  },
+    {
+        path: '/registro',
+        component: () =>
+            import ('pages/Registro.vue'),
+    },
 
+    {
+        path: '/login',
+        component: () =>
+            import ('pages/Login.vue'),
+    },
+    // Usuarios
+    {
+        path: '/inicio',
+        component: () =>
+            import ('pages/inicio.vue'),
+    },
+    {
+        path: '/perfil',
+        component: () =>
+            import ('pages/Perfil.vue'),
+    },
+    {
+        path: '/rutinas',
+        component: () =>
+            import ('pages/Rutinas.vue'),
+    },
+    {
+        path: '/rutinas/crear',
+        component: () =>
+            import ('pages/CrearRutina.vue'),
+    },
+    {
+        path: '/rutinas/ver/:slug',
+        component: () =>
+            import ('pages/VerRutina.vue'),
+    },
 
-  // Usuarios
-  {
-    path: '/inicio',
-    component: () =>
-      import ('pages/inicio.vue'),
-  },
-  {
-    path: '/perfil',
-    component: () =>
-      import ('pages/Perfil.vue'),
-  },
-  {
-    path: '/rutinas',
-    component: () =>
-      import ('pages/Rutinas.vue'),
-  },
-  {
-    path: '/rutinas/crear',
-    component: () =>
-      import ('pages/CrearRutina.vue'),
-  },
-  {
-    path: '/rutinas/ver/:slug',
-    component: () =>
-      import ('pages/VerRutina.vue'),
-  },
+    // Admin
+    {
+        path: '/admin',
+        component: () =>
+            import ('layouts/Administrador.vue'),
+        children: [{
+                path: 'ejercicios',
+                component: () =>
+                    import ('pages/EjerciciosAdmin.vue')
+            },
+            {
+                path: 'usuario',
+                component: () =>
+                    import ('pages/UsuarioAdmin.vue')
+            },
+            {
+                path: 'perfil',
+                component: () =>
+                    import ('pages/PerfilAdmin.vue')
+            },
+            {
+                path: 'crear',
+                component: () =>
+                    import ('pages/CrearEjercicio.vue')
+            },
+            {
+                path: '',
+                component: () =>
+                    import ('pages/Index.vue')
+            }
+        ]
+    },
 
-  // Admin
-  {
-    path: '/admin',
-    component: () => import('layouts/Administrador.vue'),
-    children: [
-      {path: 'ejercicios', component: () => import('pages/EjerciciosAdmin.vue')},
-      {path: 'usuario', component: () => import('pages/UsuarioAdmin.vue')},
-      {path: 'perfil', component: () => import('pages/PerfilAdmin.vue')},
-      {path: 'crear', component: () => import('pages/CrearEjercicio.vue')},
-      {path: '', component: () => import('pages/Index.vue')}
-    ]
-  },
-
-  {
-    path: '/prueba',
-    component: () => import('pages/Prueba.vue'),
-  },
+    {
+        path: '/prueba',
+        component: () =>
+            import ('pages/Prueba.vue'),
+    },
 
 ];
 
 // Always leave this as last one
 if (process.env.MODE !== 'ssr') {
-  routes.push({
-    path: '*',
-    component: () =>
-      import ('pages/Error404.vue')
-  })
+    routes.push({
+        path: '*',
+        component: () =>
+            import ('pages/Error404.vue')
+    })
 }
 
 export default routes
